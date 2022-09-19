19b5729a1574b1868a2e0fe691a130e6f5fc4d24
Jolita <jolita@test.lt>
2022-09-19 20:15:32 +0300

    m

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const app = express();
 const sqlite3 = require('sqlite3').verbose();
 
 const frontend_path = '/home/jolita/projects/FinalProject/'
+// const index_path = '/home/jolita/projects/FinalProject/Public/index.html/'
 
 
 let db = new sqlite3.Database('database.sql', (err) => {
@@ -18,6 +19,34 @@ app.listen(5000, () => {
   console.log("Server is running on port 5000");
 })
 
+
+
+
+// app.get('/', (req, response) =>{
+//
+//  response.sendFile(index_path);
+//
+// })
+//
+// app.get('/Scripts/scriptindex.js', (req, response) =>{
+//
+//  response.sendFile('/home/jolita/projects/FinalProject/Public/Scripts/scriptindex.js');
+//
+// })
+//
+//
+// app.post("/add-customer", (request, response) => {
+//
+//
+//   let sqlQuery = `INSERT INTO customers (name, lastname, phone, email
+//   ) VALUES('${request.body.name}',
+//   '${request.body.lastname}', '${request.body.phone}', '${request.body.email}')`;
+//
+//   connection.query(sqlQuery)
+//
+//   response.sendFile(index_path);
+//
+//
 
 // prompt.start();
 //
