b0eb651be3c90d87d9f0a041912c972afd5bdd8c
Jolita <jolita@test.lt>
2022-09-21 19:18:57 +0300

    db fixed

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,13 @@
 const express = require("express");
 const app = express();
 const sqlite3 = require('sqlite3').verbose();
+app.use(express.urlencoded({
+    extended: true
+}));
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+
 
 let db = new sqlite3.Database('backend/database.sql', (err) => {
   if (err) {
@@ -18,9 +25,32 @@ app.listen(5000, () => {
 
 app.post('/make_reservation', (request, response) =>{
 
- console.log(request.params)
+  let reservation = request.body
+
+  db.run("INSERT INTO reservations (firstname, lastname, email) VALUES (?,?,?)",
+    [reservation.fname, reservation.lname, reservation.email], function (err, result) {
+    if (err) throw err;
+
+    fs.readFile('Public/Reservation.html', 'utf8', function(err, data) {
+
+        if (err) throw err;
+
+        let html_file = cheerio.load(data);
+
+        html_file('.container2').replaceWith('<div>Reservation successful</div>')
+
+        response.send(html_file.html())
+
+    });
+
+
+  });
+
 
 })
+
+
+
 
 // app.get('/', (req, response) =>{
 //
