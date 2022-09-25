8a0f9f6d94b6aedacd6dfc6d8da0786cd06f317d
Jolita <jolita@test.lt>
2022-09-25 14:22:31 +0300

    remove comments

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,3 @@
-//const prompt = require('prompt');
 const express = require("express");
 const app = express();
 const sqlite3 = require('sqlite3').verbose();
@@ -55,62 +54,3 @@ app.post('/make_reservation', (request, response) => {
 
 
 })
-
-
-
-
-// app.get('/', (req, response) =>{
-//
-//  response.sendFile(index_path);
-//
-// })
-//
-// app.get('/Scripts/scriptindex.js', (req, response) =>{
-//
-//  response.sendFile('/home/jolita/projects/FinalProject/Public/Scripts/scriptindex.js');
-//
-// })
-//
-//
-// app.post("/add-customer", (request, response) => {
-//
-//
-//   let sqlQuery = `INSERT INTO customers (name, lastname, phone, email
-//   ) VALUES('${request.body.name}',
-//   '${request.body.lastname}', '${request.body.phone}', '${request.body.email}')`;
-//
-//   connection.query(sqlQuery)
-//
-//   response.sendFile(index_path);
-//
-//
-
-// prompt.start();
-//
-// prompt.get(['firstname', 'lastname'], function (err, result) {
-//   if (err) {
-//     throw err;
-//   }
-//
-//   db.run(`INSERT INTO reservations(firstname, lastname)  VALUES ('${result.firstname}','${result.lastname}')`, [], (err) =>{
-//     console.log(err);
-//   });
-//
-//   db.all('SELECT * FROM reservations', [], (err, rows) => {
-//     if (err) {
-//       throw err;
-//     }
-//     console.table(rows);
-//
-//   //  rows.forEach((row) => {
-//   //    console.log(row.firstname);
-//   //  });
-//   });
-//
-//
-//
-// });
-
-
-
-//db.close();
